8b6d1ea71b328e567b1d7b6faf2f4ad55361049e
Tenzin Norzin <47469997+tenzinnorzin@users.noreply.github.com>
2021-08-26 00:46:37 +0530

    Create secondLargest2.java

---
+++ Arrays/secondLargest2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class secondLargest2 {
+    public static void main(String [] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter size of array: ");
+        int n= sc.nextInt();
+        int [] arr= new int[n];
+        System.out.println("enter array elements: ");
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+        sLarge(arr);
+    }
+    public static void sLarge(int [] arr){
+        int largest=0, res=-1;
+        for(int i=1;i< arr.length;i++){
+            if(arr[i]>arr[largest]){
+                res=largest;
+                largest=i;
+            }
+            if (arr[i] < arr[largest]) {
+                if (res==-1 || arr[i] > arr[res]) {
+                    res = i;
+                }
+            }
+        }
+        if(res==-1){
+            System.out.println("no second largest element");
+        }
+        else
+            System.out.println("second largest element is:"+arr[res]);
+    }
+}
