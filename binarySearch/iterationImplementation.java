75a7e00f239cce7ed3c19ccb45a648295215df54
Tenzin Norzin <47469997+tenzinnorzin@users.noreply.github.com>
2021-08-25 21:30:06 +0530

    Update iterationImplementation.java

--- binarySearch/iterationImplementation.java
+++ binarySearch/iterationImplementation.java
@@ -1,3 +1,6 @@
+/* time complexity:- logn 
+auxiliary space:- O(1) */
+
 public class iterationImplementation {
     public static void main(String [] args){
         int arr[]={1,3,5,7,9,11,13,15};
