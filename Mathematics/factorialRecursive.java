28ae8b3dd047955b62375e9c7627cbf57814fd81
Tenzin Norzin <47469997+tenzinnorzin@users.noreply.github.com>
2021-08-28 13:35:50 +0530

    Create factorialRecursive.java

---
+++ Mathematics/factorialRecursive.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class factorialRecursive
+{
+	public static void main(String[] args) {
+	    Scanner sc= new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num= sc.nextInt();
+		int fact=factorial(num);
+		System.out.println(num+"! is: "+fact);
+		
+	}
+	static int factorial(int num){
+	    if(num==1 || num ==0)  return 1;
+	    return num*factorial(num-1);
+	}
+}
